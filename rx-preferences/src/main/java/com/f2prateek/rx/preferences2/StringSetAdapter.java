06e4e722e22d52aa1ae13161d8b53e2cef5ec1ca
Eric Cochran <Eric@NightlyNexus.com>
2017-03-09 17:49:34 -0800

    Return unmodifiable string set from preferences.

--- rx-preferences/src/main/java/com/f2prateek/rx/preferences2/StringSetAdapter.java
+++ rx-preferences/src/main/java/com/f2prateek/rx/preferences2/StringSetAdapter.java
@@ -3,6 +3,7 @@ package com.f2prateek.rx.preferences2;
 import android.annotation.TargetApi;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
+import java.util.Collections;
 import java.util.Set;
 
 import static android.os.Build.VERSION_CODES.HONEYCOMB;
@@ -14,7 +15,7 @@ final class StringSetAdapter implements Preference.Adapter<Set<String>> {
   @Override public Set<String> get(@NonNull String key, @NonNull SharedPreferences preferences) {
     Set<String> value = preferences.getStringSet(key, null);
     assert value != null; // Not called unless key is present.
-    return value;
+    return Collections.unmodifiableSet(value);
   }
 
   @Override public void set(@NonNull String key, @NonNull Set<String> value,
