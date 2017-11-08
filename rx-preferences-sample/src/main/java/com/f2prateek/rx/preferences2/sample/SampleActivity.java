520f858fe95ec0bf8965624dc67961c5150d0a99
Dmitriy Makarov <xcorp.one@gmail.com>
2017-11-08 05:19:35 +0300

    Resolve issue #104 (How do I save edittext values)

--- rx-preferences-sample/src/main/java/com/f2prateek/rx/preferences2/sample/SampleActivity.java
+++ rx-preferences-sample/src/main/java/com/f2prateek/rx/preferences2/sample/SampleActivity.java
@@ -4,15 +4,21 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.CheckBox;
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.f2prateek.rx.preferences2.Preference;
 import com.f2prateek.rx.preferences2.RxSharedPreferences;
 import com.jakewharton.rxbinding.widget.RxCompoundButton;
+import com.jakewharton.rxbinding.widget.RxTextView;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import hu.akarnokd.rxjava.interop.RxJavaInterop;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
-
 
 import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
@@ -20,7 +26,9 @@ public class SampleActivity extends Activity {
 
   @BindView(R.id.foo_1) CheckBox foo1Checkbox;
   @BindView(R.id.foo_2) CheckBox foo2Checkbox;
+  @BindView(R.id.text) EditText fooEditText;
   Preference<Boolean> fooPreference;
+  Preference<String> fooTextPreference;
   CompositeDisposable disposables;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,7 @@ public class SampleActivity extends Activity {
 
     // foo
     fooPreference = rxPreferences.getBoolean("foo");
+    fooTextPreference = rxPreferences.getString("fooText");
   }
 
   @Override protected void onResume() {
@@ -44,6 +53,7 @@ public class SampleActivity extends Activity {
     disposables = new CompositeDisposable();
     bindPreference(foo1Checkbox, fooPreference);
     bindPreference(foo2Checkbox, fooPreference);
+    bindPreference(fooEditText, fooTextPreference);
   }
 
   @Override protected void onPause() {
@@ -60,5 +70,16 @@ public class SampleActivity extends Activity {
     disposables.add(RxJavaInterop.toV2Observable(RxCompoundButton.checkedChanges(checkBox))
         .skip(1) // First emission is the original state.
         .subscribe(preference.asConsumer()));
+  }
+
+  void bindPreference(final EditText editText, Preference<String> preference) {
+    disposables.add(preference.asObservable()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(t -> Toast.makeText(this, t, Toast.LENGTH_SHORT).show()));
+    disposables.add(RxJavaInterop.toV2Observable(RxTextView.textChangeEvents(editText))
+            .skip(1) // First emission is the original state.
+            .debounce(500, TimeUnit.MILLISECONDS) // Filter out UI events that are emitted in quick succession.
+            .map(e -> e.text().toString())
+            .subscribe(preference.asConsumer()));
   }
 }
